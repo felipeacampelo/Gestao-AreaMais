27dd263fdc522323fbc6a95815d5159f2253e936
felipecampelo <felipeaquinocamp@gmail.com>
2025-11-04 14:44:16 -0300

    Corrige configuração de arquivos estáticos

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -103,8 +103,8 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Media files
 MEDIA_URL = 'media/'
